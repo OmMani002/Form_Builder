eafa3883ec03028fdbf14d033fc30b34b640a6bc
Om Mani Tripathi <maniom002@gmail.com>
2023-10-23 14:12:39 +0530

    couple more updates added total visits section

--- make-forms/app/(dashboard)/page.tsx
+++ make-forms/app/(dashboard)/page.tsx
@@ -1,35 +1,67 @@
 import { GetFormStats } from "@/actions/form"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { ReactNode, Suspense } from "react";
 import {LuView} from "react-icons/lu"
+
 export default function Home() {
   return (
-    <div className="container pt-4">Hello</div>
+    <div className="container pt-4">
+      <Suspense fallback={<StatsCards loading={true}/>}>
+        <CardStatsWrapper/>
+      </Suspense>
+    </div>
   )
 }
 
 async function CardStatsWrapper() {
     const stats = await GetFormStats();
-    return <StatsCard loading={false} data={stats} />
+    return <StatsCards loading={false} data={stats} />
 }
 
 interface StatsCardProps{
-  data: Awaited<ReturnType< GetFormStats>>;
+  data?: Awaited<ReturnType<typeof GetFormStats>>;
   loading: boolean;
 }
 
-function StatsCard(props: StatsCardProps){
+function StatsCards(props: StatsCardProps) {
   const {data, loading} = props;
 
   return (
     <div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
       <StatsCard title="Total visits" icon={<LuView className="text-blue-600"/>}
       helperText="All time form visits"
-      value={data.visits.toLocalString()}
+      value={data?.visits.toLocaleString() || ""}
       loading={loading}
       className="shadow-md shadow-blue-600"
       />
     </div>
-  )
+  );
 }
 
 
-function StatsCard({title, value, icon, helperText, loading, className}){}
+function StatsCard({title, value, icon, helperText, loading, className}: {
+  title: string;
+  value: string;
+  helperText: string;
+  className: string;
+  loading: boolean;
+  icon: ReactNode;
+}){
+  return <Card>
+    <CardHeader>
+      <CardTitle>
+        {title}
+      </CardTitle>
+    </CardHeader>
+    <CardContent>
+      <div className="text-2xl font-bold">
+        {
+          loading && <Skeleton>
+            <span>0</span>
+          </Skeleton>
+        }
+      </div>
+    </CardContent>
+  </Card>
+}
