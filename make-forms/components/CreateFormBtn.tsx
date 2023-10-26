2d3dfb416ca2bcc3affb638a9f674d09edc0474f
Om Mani Tripathi <maniom002@gmail.com>
2023-10-26 08:50:10 +0530

    created a new form button

--- make-forms/components/CreateFormBtn.tsx
+++ make-forms/components/CreateFormBtn.tsx
@@ -11,10 +11,24 @@ import {
     DialogTrigger,
 } from "./ui/dialog";
 
+import { BsFileEarmarkPlus } from "react-icons/bs";
+import { ImSpinner2 } from "react-icons/im";
+import { Button } from './ui/button';
+
 function CreateFormBtn() {
-  return (
-    <div>CreateFormBtn</div>
-  )
+  return <Dialog>
+    <DialogTrigger asChild>
+      <Button>Create new Form</Button>
+    </DialogTrigger>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Create Form</DialogTitle>
+        <DialogDescription>
+          Create a new form to start collecting responses.
+        </DialogDescription>
+      </DialogHeader>
+    </DialogContent>
+  </Dialog>
 }
 
 export default CreateFormBtn
